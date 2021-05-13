950dadf45d5cb3cb5272ec3115f6899de177debd
L2442 <76606422+L2442@users.noreply.github.com>
2021-05-13 18:02:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ var END = 0;
 var gameState = PLAY;
 var hitScore = 2;
 var fc;
+var life, life2, lifeImg;
 
 function preload(){
   bgImg = loadImage("/Images/bg1.jpg");
@@ -23,6 +24,7 @@ function preload(){
   towerImg = loadImage("/Images/tower.PNG");
   cloudImg = loadImage("/Images/cloud.png");
   bushImg = loadImage("/Images/bush.png");
+  lifeImg = loadImage("/Images/life.png");
 }
 
 function setup(){
@@ -37,6 +39,14 @@ function setup(){
 
  iGround = createSprite(mario.x, mario.y+90,height,5);
  iGround.visible = false;
+
+ life = createSprite(mario.x-50, height - 600,20,20);
+ life.addImage(lifeImg);
+ life.scale = 0.05;
+
+ life2 = createSprite(mario.x-30, height - 600,20,20);
+ life2.addImage(lifeImg);
+ life2.scale = 0.05;
  
  mario.setCollider("rectangle",0,0,mario.width, mario.height);
  mario.debug = true;
@@ -58,8 +68,9 @@ function draw(){
   background(rgb(94, 145, 254));
 
   camera.position.x = mario.x+500;
-  camera.position.y = mario.y-150;
-
+  //camera.position.y = mario.y-150;
+  life.x = mario.x-50;
+  life2.x = mario.x-25;
 
   if(gameState === PLAY){
   mario.velocityX = (5+score/120);
@@ -69,13 +80,26 @@ function draw(){
     bg.x = mario.x + bg.width/2-150;
   }
   mario.velocityY = 0;
-  if(keyDown("SPACE")){
+  if(keyDown("SPACE") && mario.y>height/2-50){
     mario.velocityY = -20;
   }
   mario.velocityY = mario.velocityY+5;
   mario.collide(iGround);
 
-  randomNum = Math.round(random(1,3));
+  var x = Math.round(random(1,3));
+  switch(x){
+    case 1: x = 60;
+    break;
+    case 2: x = 100;
+    break;
+    case 3: x = 140;
+    break;
+    default: break;
+  }
+  console.log(x);
+
+  if(frameCount%x === 0){
+  randomNum = Math.round(random(1,4));
   if(randomNum === 1){
     spawnObstacles();
   }
@@ -88,14 +112,14 @@ function draw(){
     spawnbb();
   }
 
+  if(randomNum === 3){
   spawnBushes();
-  mario.collide(towerGroup);
-  mario.collide(iObGrp);
+  }
+      }
   }
 
-  /*if(iTowerGrp.isTouching(mario) || obGroup.isTouching(mario)){
-    hitScore = hitScore-1;
-  }*/
+  mario.collide(towerGroup);
+  mario.collide(iObGrp);
 
   if (obGroup.isTouching(mario) || iTowerGrp.isTouching(mario) || bbGrp.isTouching(mario)) {
     fc = frameCount;
@@ -104,8 +128,13 @@ function draw(){
     hitScore = hitScore-1;
   }
 
+  if(hitScore === 1){
+    life2.visible = false;
+  }
+
   if(hitScore === 0){
     gameState = END;
+    life.visible = false;
   }
 
   if(gameState === END){
@@ -123,12 +152,11 @@ function draw(){
   drawSprites();
   textSize(20);
   fill("black");
-  text("Score: "+score,mario.x+100,mario.y-200);
-  text("Life: "+hitScore, mario.x-50, mario.y - 200);
+  text("Score: "+score,camera.position.x+100,150);
+  text("Life: ", life.x-50,life.y+10);
 }
 
 function spawnBushes(){
-  if(frameCount%140 === 0){
   bush = createSprite(mario.x+width,540,20,20);
   bush.addImage(bushImg);
   bush.scale = 0.4;
@@ -136,11 +164,9 @@ function spawnBushes(){
   mario.depth = mario.depth+1;
   bush.lifetime = width/bush.velocityX;
   bGroup.add(bush);
-  }
 }
 
 function spawnObstacles(){
-  if(frameCount%120 === 0){
     ob = createSprite(mario.x+width,height-100,20,20);
     ob.addAnimation("xyz",obImg);
     ob.scale = 3;
@@ -159,12 +185,9 @@ function spawnObstacles(){
     obGroup.add(ob1);
     obGroup.add(ob2);
     obGroup.add(ob);
-  }
-  
 }
 
 function spawnTowers(){
-  if(frameCount%120 === 0){
     tower = createSprite(mario.x+width, height-135,20,20);
     tower.addImage(towerImg);
     tower.depth = tower.depth;
@@ -174,15 +197,12 @@ function spawnTowers(){
     iTower.lifetime = width/iTower.velocityX;
     towerGroup.add(tower);
     iTowerGrp.add(iTower);
-  }
 }
 
 function spawnbb(){
-  if(frameCount%190 === 0){
     bb = createSprite(mario.x+width, height-160,20,20);
     bb.addImage(bbImg);
-    bb.scale = 0.2;
+    bb.scale = 0.15;
     bb.lifetime = width/bb.velocityX;
     bbGrp.add(bb);
-  }
 }
